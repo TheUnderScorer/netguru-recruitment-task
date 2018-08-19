2b8f07a30c1b97056d3a917d471337bcc0f5d8ba
Przemysław Żydek <przemek543@gmail.com>
2018-08-19 02:01:51 +0200

    Include example settings.js file

---
+++ settings.js
@@ -0,0 +1,23 @@
+/**
+ * Contains important server settings
+ *
+ * @file settings.js
+ * */
+
+module.exports = {
+    /**
+     * @var {Object} Stores database related properties
+     * */
+    db:  {
+        host:    process.env.dbHost || 'mongodb://localhost:27017',
+        options: {
+            dbName: 'movies_api'
+        }
+    },
+    /**
+     * @var {Object} Stores API related properties such as API keys
+     * */
+    api: {
+        omdba: process.env.omdbApiKey || 'XYZ'
+    }
+};
